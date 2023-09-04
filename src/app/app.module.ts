230d8179b4da253522b909aa6eec91de8ce856e7
vania <ivan.tym4ak@gmail.com>
2023-09-04 13:11:14 +0300

    first init fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './components/app.component';
-import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './components/user/user.component';
-import { UserDetailsComponent } from './components/user-details/user-details.component';
+import {AppComponent} from './components/app.component';
 import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UsersComponent,
-    UserComponent,
-    UserDetailsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -21,4 +15,5 @@ import {HttpClientModule} from "@angular/common/http";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
